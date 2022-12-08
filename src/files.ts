90d17af330ddacbac30e84bb9ba6c3fac766023e
Nona9614 <Nona9614@hotmail.com>
2022-12-08 12:09:58 -0600

    Adding throu sync

--- src/files.ts
+++ src/files.ts
@@ -1,11 +1,10 @@
 import * as fs from "fs";
 import * as path from "path";
 
-import * as process from 'process';
+import * as process from "process";
 import lienzo from "./lienzo";
 
-import { FgExIchor, ExDefault, FgExOrange, SpLine, FgExAmber, FgExCoral, FgExBlood, FgExString } from 'node-lienzo';
-
+import { FgExIchor, ExDefault } from "node-lienzo";
 
 export const isFolder = (value: string) => !value.includes(".");
 
@@ -14,12 +13,19 @@ export const isFolder = (value: string) => !value.includes(".");
  */
 export function assertf(...filenames: string[]) {
   for (const filename of filenames) {
-      if (!fs.existsSync(filename)) throw new Error(`File does not exists:\n${FgExIchor}${filename}${ExDefault}`);        
+    if (!fs.existsSync(filename))
+      throw new Error(
+        `File does not exists:\n${FgExIchor}${filename}${ExDefault}`
+      );
   }
 }
 
 type SearchPredicate = (pathname: string) => boolean;
-async function _search(folder: string, predicate: SearchPredicate, found: string[] = []) {
+async function _search(
+  folder: string,
+  predicate: SearchPredicate,
+  found: string[] = []
+) {
   const dirs = fs.readdirSync(folder);
   for (let i = 0; i < dirs.length; i++) {
     const dir = path.join(folder, dirs[i]);
@@ -39,13 +45,13 @@ async function _search(folder: string, predicate: SearchPredicate, found: string
 export async function search(predicate: SearchPredicate, ...folders: string[]) {
   let found: string[] = [];
   for (const folder of folders) {
-    found = await _search(folder, predicate, found)
+    found = await _search(folder, predicate, found);
   }
   return found;
 }
 
-type ThrouWatcher = (pathname: string) => Promise<void> | void
-type ThrouObserve = 'files' | 'folders' | 'both';
+type ThrouWatcher = (pathname: string) => Promise<void> | void;
+type ThrouObserve = "files" | "folders" | "both";
 async function _throu(
   pathname: string,
   watcher: ThrouWatcher,
@@ -68,6 +74,29 @@ async function _throu(
   }
 }
 
+type ThrouWatcherSync = (pathname: string) => void;
+async function _throuSync(
+  pathname: string,
+  watcher: ThrouWatcherSync,
+  observe: ThrouObserve
+) {
+  const dirs = fs.readdirSync(pathname);
+  for (let i = 0; i < dirs.length; i++) {
+    const dir = path.join(pathname, dirs[i]);
+    const _isFolder = isFolder(dir);
+    if (
+      (observe === "files" && !_isFolder) ||
+      (observe === "folders" && _isFolder) ||
+      observe === "both"
+    ) {
+      watcher(dir);
+    }
+
+    // Recursively resolve folders
+    if (_isFolder) _throuSync(dir, watcher, observe);
+  }
+}
+
 /**
  * Iterates recursively given throu the folder names
  * and executes the `watcher` every time it passes a file
@@ -76,35 +105,69 @@ async function _throu(
  * @param context.watcher Watches for each iteration
  * @default context Observes both files and folders
  */
- export async function throu(context: {
-  watcher: ThrouWatcher,
-  observe?: ThrouObserve
- }, ...folders: string[]) {
-  const observe = context.observe ? context.observe : 'both';
+export async function throu(
+  context: {
+    watcher: ThrouWatcher;
+    observe?: ThrouObserve;
+  },
+  ...folders: string[]
+) {
+  const observe = context.observe ? context.observe : "both";
   for (const folder of folders) {
     await _throu(folder, context.watcher, observe);
   }
 }
 
+/**
+ * Iterates recursively given throu the folder names
+ * and executes the `watcher` every time it passes a file
+ * @param folders The folders to search
+ * @param context
+ * @param context.watcher Watches for each iteration
+ * @default context Observes both files and folders
+ */
+export async function throuSync(
+  context: {
+    watcher: ThrouWatcherSync;
+    observe?: ThrouObserve;
+  },
+  ...folders: string[]
+) {
+  const observe = context.observe ? context.observe : "both";
+  for (const folder of folders) {
+    _throuSync(folder, context.watcher, observe);
+  }
+}
+
 function _read(filename: string, encoding?: BufferEncoding) {
   return new Promise<any>((resolve, reject) => {
-      if (!fs.existsSync(filename)) reject(Error(`File does not exists:\n${FgExIchor}${filename}${ExDefault}`));
-      else {
-          fs.readFile(filename, {
-            encoding,
-          }, (error, data) => {
-              if (error) reject(error);
-              else resolve(data);
-          });      
-      }
+    if (!fs.existsSync(filename))
+      reject(
+        Error(`File does not exists:\n${FgExIchor}${filename}${ExDefault}`)
+      );
+    else {
+      fs.readFile(
+        filename,
+        {
+          encoding,
+        },
+        (error, data) => {
+          if (error) reject(error);
+          else resolve(data);
+        }
+      );
+    }
   });
 }
 /**
  * Reads the content from a file as `String`
  * @param filename The file to be readed
  */
-export async function read(filename: string, encoding: BufferEncoding = 'utf-8') {
-  const content = await _read(filename, encoding) as string
+export async function read(
+  filename: string,
+  encoding: BufferEncoding = "utf-8"
+) {
+  const content = (await _read(filename, encoding)) as string;
   return content;
 }
 
@@ -112,8 +175,8 @@ export async function read(filename: string, encoding: BufferEncoding = 'utf-8')
  * Extracts the content from a file as `Buffer`
  * @param filename The file to be readed
  */
- export async function extract(filename: string) {
-  const content = await _read(filename) as Buffer;
+export async function extract(filename: string) {
+  const content = (await _read(filename)) as Buffer;
   return content;
 }
 
@@ -143,7 +206,7 @@ export function mkdir(pathname: string) {
  * @param pathname The relative path to create the folder
  * @remarks If a filename is passed, a folder with the pathname will be created
  */
- export function mkdirSync(pathname: string) {
+export function mkdirSync(pathname: string) {
   const route = isFolder(pathname) ? pathname : path.dirname(pathname);
   if (fs.existsSync(route)) return;
   fs.mkdirSync(route);
@@ -162,8 +225,8 @@ function _write(filename: string, data: string | Buffer, append = false) {
       (error) => {
         if (error) reject(error);
         else {
-            lienzo.null("File created").line().string(filename).line().print();
-            resolve();
+          lienzo.null("File created").line().string(filename).line().print();
+          resolve();
         }
       }
     );
@@ -186,38 +249,63 @@ export function readSync(filename: string) {
   return fs.readFileSync(filename, { encoding: "utf-8" });
 }
 
-export async function write(filename: string, data: string | Buffer, append = false) {
+export async function write(
+  filename: string,
+  data: string | Buffer,
+  append = false
+) {
   await mkdir(filename);
   await _write(filename, data, append);
 }
 
-async function _copy(from: string, to: string, append = false, fallback = "copy", recursions = 0) {
-
+async function _copy(
+  from: string,
+  to: string,
+  append = false,
+  fallback = "copy",
+  recursions = 0
+) {
   const isFromFolder = isFolder(from);
   const isToFolder = isFolder(to);
 
-  if (recursions >= 50) throw new Error(`${recursions} recursions reached, process stop before memory leak`);
+  if (recursions >= 50)
+    throw new Error(
+      `${recursions} recursions reached, process stop before memory leak`
+    );
 
-  if (!(from && to)) throw new Error(`Not valid values input paths for copy:\nfrom:\n${from}\nto:\n${to}\n`); 
-  const _fallback = path.isAbsolute(fallback) ? fallback : path.resolve(process.cwd(), fallback);
+  if (!(from && to))
+    throw new Error(
+      `Not valid values input paths for copy:\nfrom:\n${from}\nto:\n${to}\n`
+    );
+  const _fallback = path.isAbsolute(fallback)
+    ? fallback
+    : path.resolve(process.cwd(), fallback);
 
   if (isFromFolder && isToFolder) {
-      await mkdir(to);
-      const dirs = fs.readdirSync(from);
-      for (const dir of dirs) {
-          await _copy(path.join(from, dir), path.join(to, dir), append, _fallback, recursions + 1);
-      }
+    await mkdir(to);
+    const dirs = fs.readdirSync(from);
+    for (const dir of dirs) {
+      await _copy(
+        path.join(from, dir),
+        path.join(to, dir),
+        append,
+        _fallback,
+        recursions + 1
+      );
+    }
   } else if (!isFromFolder && !isToFolder) {
-      const data = await _read(from);
-      await _write(to, data, append);
+    const data = await _read(from);
+    await _write(to, data, append);
   } else if (!isFromFolder && isToFolder) {
-      const _to = path.isAbsolute(to) ? to : path.resolve(_fallback, to);
-      await mkdir(_to);
-      const name = path.basename(from);
-      const data = await _read(from);
-      await _write(path.resolve(_to, name), data, append);
-  } else if (isFromFolder && !isToFolder)  {
-      throw Error(`If 'from' is a folder, 'to' cannot be a file:\nfrom:\n${from}\nto:\n${to}\n`);
+    const _to = path.isAbsolute(to) ? to : path.resolve(_fallback, to);
+    await mkdir(_to);
+    const name = path.basename(from);
+    const data = await _read(from);
+    await _write(path.resolve(_to, name), data, append);
+  } else if (isFromFolder && !isToFolder) {
+    throw Error(
+      `If 'from' is a folder, 'to' cannot be a file:\nfrom:\n${from}\nto:\n${to}\n`
+    );
   }
 }
 
@@ -228,7 +316,12 @@ async function _copy(from: string, to: string, append = false, fallback = "copy"
  * @param append If set to true the content will be attached instead of overwrite the files
  * @param fallback A fallback folder in case no folder fo place the copied content is found
  */
-export function copy(from: string, to: string, append = false, fallback = "copy") {
+export function copy(
+  from: string,
+  to: string,
+  append = false,
+  fallback = "copy"
+) {
   return _copy(from, to, append, fallback);
 }
 
@@ -238,38 +331,51 @@ export function copy(from: string, to: string, append = false, fallback = "copy"
  */
 export function destroy(filename: string) {
   return new Promise<void>((resolve, reject) => {
-      fs.rm(filename, {
-          recursive: true
-      }, function (error) {
-          if (!fs.existsSync(filename)) resolve();
-          else if (error) reject(error)
-          else resolve();
-      })
-  })
+    fs.rm(
+      filename,
+      {
+        recursive: true,
+      },
+      function (error) {
+        if (!fs.existsSync(filename)) resolve();
+        else if (error) reject(error);
+        else resolve();
+      }
+    );
+  });
 }
 
 function _empty(folder: string) {
   return new Promise<void>((resolve, reject) => {
-      fs.rm(folder, {
-          recursive: true
-      }, function (error) {
-          if (!fs.existsSync(folder)) resolve();
-          else if (error) reject(error)
-          else resolve();
-      })
-  })
+    fs.rm(
+      folder,
+      {
+        recursive: true,
+      },
+      function (error) {
+        if (!fs.existsSync(folder)) resolve();
+        else if (error) reject(error);
+        else resolve();
+      }
+    );
+  });
 }
 
 /**
  * Finds synchronously a file with possible extensions withing a route
  * @returns the path if found otherwise `null`
  */
-export function findSync(route: string, filename: string, extensions: string[]) {
-  const tries: string[] = []
+export function findSync(
+  route: string,
+  filename: string,
+  extensions: string[]
+) {
+  const tries: string[] = [];
   for (const extension of extensions) {
     const posibility = path.join(route, filename) + extension;
     tries.push(posibility);
-    if (fs.existsSync(posibility)) return { extension, found: posibility, tries };
+    if (fs.existsSync(posibility))
+      return { extension, found: posibility, tries };
   }
   return { extensions, found: null, tries };
 }
@@ -286,5 +392,5 @@ export async function empty(folder: string, create: boolean) {
 
 /** Resolves folders and uses as base the current working working directory */
 export function resolve(...paths: string[]) {
-  return path.resolve.apply(path, [process.cwd(), ...paths])
+  return path.resolve.apply(path, [process.cwd(), ...paths]);
 }
